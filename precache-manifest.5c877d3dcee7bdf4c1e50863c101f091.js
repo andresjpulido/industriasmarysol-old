68307ffc99a089d5774730963293d9c095a57958
Andres <andresjpulido@gmail.com>
2020-04-03 11:15:27 +1300

    initial version

---
+++ precache-manifest.5c877d3dcee7bdf4c1e50863c101f091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8a707735cc52f171f1f52e07fb48c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66f3fd87f42a0409d2dc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "16b88493ede8f159a651",
+    "url": "/static/css/main.0cb1aea1.chunk.css"
+  },
+  {
+    "revision": "66f3fd87f42a0409d2dc",
+    "url": "/static/js/2.4740faf4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4740faf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b88493ede8f159a651",
+    "url": "/static/js/main.010efb04.chunk.js"
+  },
+  {
+    "revision": "f7207c20e290e29d46f9",
+    "url": "/static/js/runtime-main.daf1a92a.js"
+  }
+]);
